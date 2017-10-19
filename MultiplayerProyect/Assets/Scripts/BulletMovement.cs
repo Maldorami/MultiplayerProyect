dd8700625aaaad30c95d6fde3b69d9eebb7a9195
maldo <ramamaldonado@gmail.com>
2017-10-18 22:21:41 -0300

    BulletDamage

--- MultiplayerProyect/Assets/Scripts/BulletMovement.cs
+++ MultiplayerProyect/Assets/Scripts/BulletMovement.cs
@@ -14,4 +14,16 @@ public class BulletMovement : MonoBehaviour
         //Lifetime
         Destroy(gameObject, lifeTime);
     }
+
+    void OnCollisionEnter(Collision collision)
+    {        
+        var hit = collision.gameObject;
+        var health = hit.GetComponent<Health>();
+        if (health != null)
+        {
+            health.TakeDamage(10);
+        }
+
+        //Destroy(gameObject);
+    }
 }
